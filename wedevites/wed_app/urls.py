a286b2f075e143ae0e7914f3836c6647133db3e0
iodpoi <39857492+jpdpueyo@users.noreply.github.com>
2018-08-23 22:18:23 +0400

    Update urls.py

--- wedevites/wed_app/urls.py
+++ wedevites/wed_app/urls.py
@@ -5,7 +5,7 @@ from wed_app.views import (login_view)
 
 urlpatterns = [
     url(r'^$',views.JoiePaulofLoveView.as_view(),name='index'),
-    url(r'^JoiePaulofLove/$',views.JoiePaulofLoveView.as_view(),name='JoiePaulofLove'),
+    url(r'^joiepauloflove/$',views.JoiePaulofLoveView.as_view(),name='JoiePaulofLove'),
     url(r'^rsvp/$',views.RSVPView.as_view(),name='rsvp'),
     url(r'^rsvpform/$',views.guests,name='rsvpform'),
     url(r'^login/$',views.login_view,name='login'),
